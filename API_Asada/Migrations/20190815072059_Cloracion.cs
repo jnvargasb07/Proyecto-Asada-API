044d8570f146f26c04792b1f3b228fac18aa3d4a
Jaime A. Esquivel Gomez <jaimeesquivelgomez@gmail.com>
2019-08-15 02:11:38 -0600

    added first commit and first migration

---
+++ API_Asada/Migrations/20190815072059_Cloracion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_Asada.Migrations
+{
+    public partial class Cloracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cloracion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    fecha = table.Column<DateTime>(nullable: false),
+                    hora = table.Column<DateTime>(nullable: false),
+                    ppm = table.Column<double>(nullable: false),
+                    sector = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cloracion", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cloracion");
+        }
+    }
+}
---
+++ API_Asada/Models/Cloracion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Asada.Models
+{
+    public class Cloracion
+    {
+        public int Id { get; set; }
+        public DateTime fecha { get; set; }
+        public DateTime hora { get; set; }
+        public double ppm { get; set; }
+        public char sector { get; set; }
+    }
+}
